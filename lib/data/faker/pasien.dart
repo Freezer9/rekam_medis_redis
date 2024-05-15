017287c229169c58ccb39040c45022877d730afc
rn <rn@gmail.com>
2024-05-15 21:02:20 +0700

    UI: Riwayat Rekam Medis Pasien

--- lib/data/faker/pasien.dart
+++ lib/data/faker/pasien.dart
@@ -4,27 +4,31 @@ final List<Map<String, String>> patientsData = [
       'id': '3122600091',
       'date': 'Selasa, 16 April 2024',
       'doc' : 'Dr. Syahrul Sumono',
-      'doctitle': 'General Practicioner'
+      'doctitle': 'General Practicioner',
+      'diagnos' : 'batuk'
     },
     {
       'name': 'Roni Surya',
       'id': '3122600099',
       'date': 'Selasa, 12 April 2024',
       'doc' : 'Dr. Syahrul Sumono',
-      'doctitle': 'General Practicioner'
+      'doctitle': 'General Practicioner',
+      'diagnos' : 'demam'
     },
     {
       'name': 'Dafino Akbar',
       'id': '312260095',
       'date': 'Selasa, 10 April 2024',
       'doc' : 'Dr. Syahrul Sumono',
-      'doctitle': 'General Practicioner'
+      'doctitle': 'General Practicioner',
+      'diagnos' : 'ruam'
     },
      {
       'name': 'John Manulang',
       'id': '3122600091',
       'date': 'Rabu, 8 Mei 2024',
       'doc' : 'Dr. Ayub Wicaksono',
-      'doctitle': 'Cardiologist'
+      'doctitle': 'Cardiologist',
+      'diagnos' : 'otot ketarik'
     },
   ];
