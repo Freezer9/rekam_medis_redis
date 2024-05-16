c2a2d5b504f34b0afa877e3d52d85d7bab385880
ndo-fer <fernandomichael00@gmail.com>
2024-05-16 19:17:11 +0700

    final dari lupa password

---
+++ lib/datafake/user.dart
@@ -0,0 +1,17 @@
+final List<Map<String, String>> usersData = [
+  {
+    'dr_name': 'Dr. John Manulang',
+    'date': 'Selasa, 16 April 2024',
+    'diagnosa': 'Batuk, Pilek, Demam'
+  },
+  {
+    'dr_name': 'Dr. Roni Surya',
+    'date': 'Selasa, 12 April 2024',
+    'diagnosa': 'Hepatitis'
+  },
+  {
+    'dr_name': 'Dr. Dafino Akbar',
+    'date': 'Selasa, 10 April 2024',
+    'diagnosa': 'Hepatitis'
+  },
+];
--- lib/forget_pw.dart
+++ lib/forget_pw.dart
@@ -7,7 +7,8 @@ class ResetPasswordPage extends StatefulWidget {
 
 class _ResetPasswordPageState extends State<ResetPasswordPage> {
   final TextEditingController newPasswordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   bool _obscureTextNew = true;
   bool _obscureTextConfirm = true;
 
@@ -16,141 +17,154 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Reset Password"),
+        title: Text("Lupa Password"),
         backgroundColor: Color(0xFFA2C9FE),
         centerTitle: true,
       ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                child: Image.asset(
-                  "assets/background.png",
-                  fit: BoxFit.fill,
-                ),
-              ),
-              Container(
-                width: width * 0.9,
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1.0, color: Color(0xffC3C6CF)),
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      spreadRadius: 0.1,
-                      blurRadius: 1,
-                      color: Color(0xFF38608F),
-                    ),
-                  ],
-                ),
-                padding: EdgeInsets.all(25.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "Atur Ulang Password-mu",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.black,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    buildPasswordField(
-                      'Masukkan Password Baru', 
-                      newPasswordController, 
-                      _obscureTextNew,
-                      (newValue) {
-                        setState(() {
-                          _obscureTextNew = newValue;
-                        });
-                      }
-                    ),
-                    SizedBox(height: 20), // Menambahkan padding antara dua text fields
-                    buildPasswordField(
-                      'Masukkan Ulang Password Baru', 
-                      confirmPasswordController, 
-                      _obscureTextConfirm,
-                      (newValue) {
-                        setState(() {
-                          _obscureTextConfirm = newValue;
-                        });
-                      }
-                    ),
-                    SizedBox(height: 40),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        // Implement password reset logic here
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green, // Background color
-                        foregroundColor: Colors.white, // Text color
-                        minimumSize: Size(double.infinity, 50), // Button size
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30), // Rounded button
-                        ),
-                      ),
-                      icon: Icon(Icons.lock_reset),
-                      label: Text(
-                        'PERBARUI PASSWORD',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            top: 20,
-            child: Stack(
-              alignment: Alignment.center,
+      body: SingleChildScrollView(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Column(
               children: [
                 Container(
-                  width: 130,
-                  height: 130,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 2),
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 2,
-                        blurRadius: 10,
-                        color: Colors.black.withOpacity(0.2),
-                      ),
-                    ],
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                        'https://cdn.pixabay.com/photo/2024/01/25/03/16/capuchin-monkey-8530884_640.jpg',
-                      ),
-                    ),
+                  width: double.infinity,
+                  child: Image.asset(
+                    "assets/background.png",
+                    fit: BoxFit.fill,
                   ),
                 ),
                 Container(
-                  width: 130,
-                  height: 130,
+                  width: width * 0.9,
                   decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.5),
-                    shape: BoxShape.circle,
+                    border: Border.all(width: 1.0, color: Color(0xffC3C6CF)),
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        spreadRadius: 0.2,
+                        blurRadius: 2,
+                        color: Color(0xFF38608F),
+                      ),
+                    ],
+                  ),
+                  padding: EdgeInsets.all(25.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Atur Ulang Password-mu",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      buildPasswordField(
+                        'Masukkan Password Baru',
+                        newPasswordController,
+                        _obscureTextNew,
+                        (newValue) {
+                          setState(() {
+                            _obscureTextNew = newValue;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                          height:
+                              20), // Menambahkan padding antara dua text fields
+                      buildPasswordField(
+                        'Masukkan Ulang Password Baru',
+                        confirmPasswordController,
+                        _obscureTextConfirm,
+                        (newValue) {
+                          setState(() {
+                            _obscureTextConfirm = newValue;
+                          });
+                        },
+                      ),
+                      SizedBox(height: 40),
+                      Container(
+                        width: 290,
+                        height: 40,
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            // Assume password reset logic here is successful
+                            _showSuccessDialog();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                Color(0xFFFCE186), // Background color
+                            foregroundColor: Colors.black, // Text color
+                            minimumSize: Size(280, 40), // Button size
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(25), // Rounded button
+                            ),
+                          ),
+                          icon: Icon(Icons.lock_reset),
+                          label: Text(
+                            'PERBARUI PASSWORD',
+                            style: TextStyle(
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Icon(Icons.lock, color: Colors.white, size: 40),
               ],
             ),
-          ),
-        ],
+            Positioned(
+              top: 20,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          color: Colors.black.withOpacity(0.1),
+                        ),
+                      ],
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          'https://cdn.pixabay.com/photo/2024/01/25/03/16/capuchin-monkey-8530884_640.jpg',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  Icon(Icons.lock,
+                      color: Color.fromARGB(255, 89, 57, 137), size: 80),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Widget buildPasswordField(
-      String labelText, TextEditingController controller, bool obscureText, ValueChanged<bool> onObscureTextChanged) {
+  Widget buildPasswordField(String labelText, TextEditingController controller,
+      bool obscureText, ValueChanged<bool> onObscureTextChanged) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -158,8 +172,8 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
           labelText,
           style: TextStyle(
             color: Colors.black,
-            fontWeight: FontWeight.w400,
-            fontSize: 16,
+            fontWeight: FontWeight.w300, // Menjadi lebih tipis
+            fontSize: 14,
           ),
         ),
         SizedBox(height: 10),
@@ -171,8 +185,8 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
             borderRadius: BorderRadius.circular(10),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                spreadRadius: 2,
+                color: Colors.black.withOpacity(0.15),
+                spreadRadius: 1,
                 blurRadius: 4,
               ),
             ],
@@ -181,17 +195,19 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
             controller: controller,
             obscureText: obscureText,
             decoration: InputDecoration(
-              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 15, horizontal: 15),
               border: InputBorder.none,
               suffixIcon: IconButton(
-                icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility),
+                icon:
+                    Icon(obscureText ? Icons.visibility_off : Icons.visibility),
                 onPressed: () {
                   onObscureTextChanged(!obscureText);
                 },
               ),
             ),
             style: TextStyle(
-              fontSize: 16,
+              fontSize: 14,
               color: Colors.black,
             ),
           ),
@@ -199,6 +215,90 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
       ],
     );
   }
+
+  void _showSuccessDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          width: 400,
+          child: Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Container(
+              width: 320,
+              height: 180,
+              padding: EdgeInsets.all(25),
+              child: Column(
+                children: [
+                  Container(
+                    width: 320,
+                    height: 10,
+                    child: Image.asset(
+                      "assets/background.png",
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.lock_person, size: 60, color: Colors.black),
+                      SizedBox(width: 5),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "PASSWORD DIPERBARUI",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            "Silakan masuk kembali.",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 280,
+                    height: 40,
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        // Navigate to login page
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFFCE186),
+                        foregroundColor: Colors.black,
+                        minimumSize: Size(280, 30),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      icon: Icon(Icons.login),
+                      label: Text(
+                        'MASUK',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
-
-
