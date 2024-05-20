bab380ef1e44269804af51487be5954cfd003118
rn <rn@gmail.com>
2024-05-20 10:42:12 +0700

    themes

---
+++ lib/themes.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+
+ const Color surfaceTint = Color(0xFF38608F); 
+ const Color containerColor = Color(0xFFB8D9F3);
+ const Color buttonColor = Color(0xFF5195D6);
+ const Color primaryFixedColor = Color(0xFF001C37);
+ const Color outButtonColor = Color(0xFFFB5F5F);
+ const Color secondaryContainerColor = Color(0xFFFCE186);
+ const Color thirdContainerColor = Color(0xFFDFD5EC);
+ const Color onSurfaceVariantColor = Color(0xFF43474E);
+ const Color appBarColor = Color(0xFFA2C9FE);
