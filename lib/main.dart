f1b6d20b8e396e5cc2284b7ba4dbf60f402c9717
Freezer9 <farismuhammadabd@gmail.com>
2024-06-27 09:25:35 +0700

    fix: environment key

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:rekam_medis_redis/constant/string.dart';
 import 'package:rekam_medis_redis/core/routes.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   await Supabase.initialize(
-    url: supabaseUrl,
-    anonKey: supabaseKey,
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env["SUPABASE_KEY"]!,
   );
 
   SystemChrome.setPreferredOrientations([
